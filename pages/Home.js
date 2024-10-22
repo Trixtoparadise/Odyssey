79da50d534d7d6bb1358defbeb48fe140f204458
Trixtoparadise <hlamolo.mhlanga7@gmail.com>
2024-10-22 14:54:27 +0200

    Modifying home screen

--- pages/Home.js
+++ pages/Home.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as SecureStore from 'expo-secure-store';
-import { Divider } from '@rneui/themed';
+import { Divider, Button } from '@rneui/themed';
 import { View, StyleSheet, Text, ActivityIndicator } from 'react-native';
 import CustomHeading from '../components/CustomHeading';
 import AccountInfo from '../components/AccountInfo';
@@ -16,7 +16,7 @@ class User {
 
 const Home = ({ navigation }) => {
   const [user, setUser] = React.useState(new User("", 0, 0));
-  const [acc, setAcc] = React.useState([]);
+  const [acc, setAcc] = React.useState();
   
   React.useEffect(() => {
     async function handleUserData () {
@@ -42,6 +42,7 @@ const Home = ({ navigation }) => {
         });
         
         const data = await response.json();
+        
         setUser(data);
       } catch(e) {
         console.log(e)
@@ -50,7 +51,7 @@ const Home = ({ navigation }) => {
 
     handleUserData();
   }, []);
-  
+
   if ( user.id != 0) {
     const handleAccountData = async () => {
       let headersList = {
@@ -69,76 +70,104 @@ const Home = ({ navigation }) => {
           headers: headersList
         });
 
-        let data = await response.json();
-        console
-        setAcc(data);
+        let data = await response.text();
+        
+        setAcc(JSON.parse(data));
       } catch (e) {
         console.log(e);
       }
     } 
 
   handleAccountData();
-
-  if (acc == [] ){
-    return (
-      <View style={{backgroundColor: 'white', width: '100%', height: '100%'}}>
-        <Divider width={1} style={{ marginTop: 12, opacity: 10}} />
-        <View style={{ marginHorizontal: 30, marginTop: 180, height: '80%'}}>
-          <CustomHeading Title={'Welcome to Odyssey Banking :)' + "\n" + 'Please click the button' + "\n" +'below to open a new account'}/>
-          <ExpansionButton buttonTitle='Open new account' buttonWidth={180} ToWhere={() => navigation.navigate('OpenAccount') }/>
+  
+  if (acc != undefined) {
+    if (acc.accounts.length == 0 && acc.beneficiaries.length == 0){
+      return (
+        <View style={{backgroundColor: 'white', width: '100%', height: '100%'}}>
+          <Divider width={1} style={{ marginTop: 12, opacity: 10}} />
+          <View style={{ marginHorizontal: 30, marginTop: 180, height: '80%'}}>
+            <CustomHeading Title={'Welcome to Odyssey Banking :)' + "\n" + 'Please click the button' + "\n" +'below to open a new account'}/>
+            <ExpansionButton buttonTitle='Open new account' buttonWidth={180} ToWhere={() => navigation.navigate('OpenAccount') }/>
+          </View>
         </View>
-      </View>
-    );
-  } else if (acc.accounts != undefined || acc.beneficiaries != undefined) {
-    let listAccounts;
-    let listBeneficiaries;
+      );
+    } else if (acc.accounts.length != 0 || acc.beneficiaries.length != 0) {
+      let listAccounts;
+      let listBeneficiaries;
+      let exBenbutton;
 
-    let mod = acc.accounts.slice(0,2);
-    let mod2 = acc.beneficiaries.slice(0,2);
-    
-    if (mod) {
-      listAccounts = mod.map((account) =>
-        <AccountInfo key={account.Account_number} Account={"Mr " + account.First_name + " " + account.Last_name + " (" + account.Account_Type + " Account) " + account.Account_number} ToWhere={() => navigation.navigate('ViewAccount', {Account_holder_Id: account.Account_holder_Id, Account_holder: "Mr " + account.First_name + " " + account.Last_name, Account_number: account.Account_number, Account_Type: account.Account_Type, Balance: account.Balance})}/>
+      let mod = acc.accounts.slice(0,2);
+      let mod2 = acc.beneficiaries.slice(0,2);
+      
+      if (mod.length != 0) {
+        listAccounts = mod.map((account) =>
+          <AccountInfo key={account.Account_number} Account={"Mr " + account.First_name + " " + account.Last_name + " (" + account.Account_Type + " Account) " + account.Account_number} ToWhere={() => navigation.navigate('ViewAccount', {Account_holder_Id: account.Account_holder_Id, Account_holder: "Mr " + account.First_name + " " + account.Last_name, Account_number: account.Account_number, Account_Type: account.Account_Type, Balance: account.Balance})}/>
+        );
+        exAccbutton = <ExpansionButton buttonTitle='All accounts' buttonWidth={120} ToWhere={() => navigation.navigate('Accounts') }/>;
+      }
+  
+      if (mod2.length != 0) {
+        listBeneficiaries = mod2.map((beneficiary) =>
+          <AccountInfo key={beneficiary.Account_number} Account={beneficiary.Beneficiary_name + " (" +  beneficiary.Bank + ") " + '\n' + beneficiary.Account_number} ToWhere={() => navigation.navigate('ViewBeneficiary', {User_Id: beneficiary.User_ID, Account_holder: beneficiary.Beneficiary_name, Bank: beneficiary.Bank, Account_number: beneficiary.Account_number})}/>
+        );
+        exBenbutton = <ExpansionButton buttonTitle='All beneficiaries' buttonWidth={140} ToWhere={() => navigation.navigate('Beneficiaries') }/>;
+      } else {
+        listBeneficiaries = <Text style={{ marginHorizontal: 30, marginTop: 20, fontSize: 30, color: 'rgba(0, 44, 106, 255)', fontStyle: 'bold' }}>No Beneficiaries</Text>;
+        exBenbutton = <Button 
+                        title={"Add new beneficiary"}
+                        icon={{
+                        name: 'pluscircleo',
+                        type: 'antdesign',
+                        size: 12,
+                        color: 'rgba(0, 44, 106, 255)',
+                        }}
+                        onPress={() => navigation.navigate('CreateBeneficiary')}
+                        type="clear"
+                        containerStyle={{
+                        width: 150,
+                        alignSelf: 'left',
+                        paddingTop: 10,
+                        marginLeft: 10,
+                        }}
+                        titleStyle={{ 
+                        color: 'rgba(0, 44, 106, 255)', 
+                        fontSize: 14 
+                        }}
+                    />
+      }
+      
+      return (
+        <View style={{ backgroundColor: 'white', width: '100%', height: '100%' }}>  
+  
+          <Divider width={1} style={{ marginTop: 12, opacity: 10}} />
+          <View style={{ marginHorizontal: 30, marginTop: 20  }}>     
+  
+            <CustomHeading Title='Accounts'/>
+            {listAccounts}
+            {exAccbutton}
+          </View>
+  
+          <View style={{ marginHorizontal: 30, marginTop: 20 }}>
+            <CustomHeading Title='Beneficiaries' />
+            {listBeneficiaries}
+            {exBenbutton}
+          </View>
+          </View>
       );
     }
-
-    if (mod2) {
-      listBeneficiaries = mod2.map((beneficiary) =>
-        <AccountInfo key={beneficiary.Account_number} Account={beneficiary.Beneficiary_name + " (" +  beneficiary.Bank + ") " + '\n' + beneficiary.Account_number} ToWhere={() => navigation.navigate('ViewBeneficiary', {User_Id: beneficiary.User_ID, Account_holder: beneficiary.Beneficiary_name, Bank: beneficiary.Bank, Account_number: beneficiary.Account_number})}/>
-      );
+    } else {
+      return (
+        <View style={{backgroundColor: 'white', width: '100%', height: '100%' }}>
+          <Divider width={1} style={{ marginTop: 12, opacity: 10}} />
+          <View style={styles.splashContainer}>
+              <Text style={{marginHorizontal: 30, fontSize: 22, marginBottom: 20, marginTop: -80, alignSelf: 'center', color: 'rgba(0, 44, 106, 255)'}}>
+                  Please Wait
+              </Text>
+              <ActivityIndicator size="large" color="rgba(0, 44, 106, 255)" />
+          </View>
+        </View>
+      )
     }
-    
-    return (
-      <View style={{ backgroundColor: 'white', width: '100%', height: '100%' }}>  
-
-        <Divider width={1} style={{ marginTop: 12, opacity: 10}} />
-        <View style={{ marginHorizontal: 30, marginTop: 20  }}>     
-
-          <CustomHeading Title='Accounts'/>
-          {listAccounts}
-          <ExpansionButton buttonTitle='All accounts' buttonWidth={120} ToWhere={() => navigation.navigate('Accounts') }/>
-        </View>
-
-        <View style={{ marginHorizontal: 30, marginTop: 20 }}>
-          <CustomHeading Title='Beneficiaries' />
-          {listBeneficiaries}
-          <ExpansionButton buttonTitle='All beneficiaries' buttonWidth={140} ToWhere={() => navigation.navigate('Beneficiaries') }/>
-        </View>
-        </View>
-    );
-  }
-  } else {
-    return (
-      <View style={{backgroundColor: 'white', width: '100%', height: '100%' }}>
-        <Divider width={1} style={{ marginTop: 12, opacity: 10}} />
-        <View style={styles.splashContainer}>
-            <Text style={{marginHorizontal: 30, fontSize: 22, marginBottom: 20, marginTop: -80, alignSelf: 'center', color: 'rgba(0, 44, 106, 255)'}}>
-                Please Wait
-            </Text>
-            <ActivityIndicator size="large" color="rgba(0, 44, 106, 255)" />
-        </View>
-      </View>
-    )
   }
 }
 
